a7bfe78474b3973beaa6d52aa91b06de01ca56d5
Sergio-Source-code <sergiosanzescallon@gmail.com>
2022-03-30 00:55:52 -0500

    Improved formatting

--- js/main.js
+++ js/main.js
@@ -21,25 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
      ScheduleDB.onsuccess = function() {
           // console.log('Database Ready');
 
-          
+
           DB = ScheduleDB.result;
 
           showConsultations();
      }
 
-   
-     ScheduleDB.onupgradeneeded = function(e) {
-          
-          let db = e.target.result;
-          
-          let objectStore = db.createObjectStore('consultations', { keyPath: 'key', autoIncrement: true } );
 
-        
-          objectStore.createIndex('patientname', 'patientname', { unique: false } );
-          objectStore.createIndex('contact', 'contact', { unique: false } );
-          objectStore.createIndex('date', 'date', { unique: false } );
-          objectStore.createIndex('time', 'time', { unique: false } );
-          objectStore.createIndex('symptoms', 'symptoms', { unique: false } );
+     ScheduleDB.onupgradeneeded = function(e) {
+
+          let db = e.target.result;
+
+          let objectStore = db.createObjectStore('consultations', { keyPath: 'key', autoIncrement: true });
+
+
+          objectStore.createIndex('patientname', 'patientname', { unique: false });
+          objectStore.createIndex('contact', 'contact', { unique: false });
+          objectStore.createIndex('date', 'date', { unique: false });
+          objectStore.createIndex('time', 'time', { unique: false });
+          objectStore.createIndex('symptoms', 'symptoms', { unique: false });
 
           //console.log('Database ready and fields created!');
      }
@@ -49,19 +49,19 @@ document.addEventListener('DOMContentLoaded', () => {
      function addConsultations(e) {
           e.preventDefault();
           let newConsultation = {
-               patientname : patientName.value,
-               
-             contact : contact.value,
-               date : date.value,
-            time : time.value,
-               symptoms : symptoms.value
+               patientname: patientName.value,
+
+               contact: contact.value,
+               date: date.value,
+               time: time.value,
+               symptoms: symptoms.value
           }
-          
+
           let transaction = DB.transaction(['consultations'], 'readwrite');
           let objectStore = transaction.objectStore('consultations');
 
           let request = objectStore.add(newConsultation);
-                    request.onsuccess = () => {
+          request.onsuccess = () => {
                form.reset();
           }
           transaction.oncomplete = () => {
@@ -70,27 +70,27 @@ document.addEventListener('DOMContentLoaded', () => {
                showConsultations();
           }
           transaction.onerror = () => {
-              //console.log();
+               //console.log();
           }
 
      }
      function showConsultations() {
-       
-          while(consultations.firstChild) {
-            consultations.removeChild(consultations.firstChild);
+
+          while (consultations.firstChild) {
+               consultations.removeChild(consultations.firstChild);
           }
-         
+
           let objectStore = DB.transaction('consultations').objectStore('consultations');
 
-          objectStore.openCursor().onsuccess = function(e) {
-               
+          objectStore.openCursor().onsuccess = function (e) {
+
                let cursor = e.target.result;
-               if(cursor) {
+               if (cursor) {
                     let ConsultationHTML = document.createElement('li');
                     ConsultationHTML.setAttribute('data-consultation-id', cursor.value.key);
                     ConsultationHTML.classList.add('list-group-item');
-                    
-                 
+
+
                     ConsultationHTML.innerHTML = `  
                          <p class="font-weight-bold">Patient Name:  <span class="font-weight-normal">${cursor.value.patientname}<span></p>
                           <p class="font-weight-bold">Contact:  <span class="font-weight-normal">${cursor.value.contact}<span></p>
@@ -99,57 +99,57 @@ document.addEventListener('DOMContentLoaded', () => {
                          <p class="font-weight-bold">Symptoms:  <span class="font-weight-normal">${cursor.value.symptoms}<span></p>
                     `;
 
-                    
+
                     const cancelBtn = document.createElement('button');
                     cancelBtn.classList.add('btn', 'btn-danger');
                     cancelBtn.innerHTML = 'Cancel';
                     cancelBtn.onclick = removeConsultation;
-               
-                 
+
+
                     ConsultationHTML.appendChild(cancelBtn);
-                 consultations.appendChild(ConsultationHTML);
+                    consultations.appendChild(ConsultationHTML);
 
                     cursor.continue();
                } else {
-                    if(!consultations.firstChild) {
-                        services.textContent = 'Change your visiting hours';
+                    if (!consultations.firstChild) {
+                         services.textContent = 'Change your visiting hours';
                          let noSchedule = document.createElement('p');
                          noSchedule.classList.add('text-center');
                          noSchedule.textContent = 'No results Found';
-                      consultations.appendChild(noSchedule);
+                         consultations.appendChild(noSchedule);
                     } else {
-                        services.textContent = 'Cancel Your consultations'
+                         services.textContent = 'Cancel Your consultations'
                     }
                }
           }
      }
 
-          function removeConsultation(e) {
-       
-          let scheduleID = Number( e.target.parentElement.getAttribute('data-consultation-id') );
-         
+     function removeConsultation(e) {
+
+          let scheduleID = Number(e.target.parentElement.getAttribute('data-consultation-id'));
+
           let transaction = DB.transaction(['consultations'], 'readwrite');
           let objectStore = transaction.objectStore('consultations');
-         
+
           objectStore.delete(scheduleID);
 
           transaction.oncomplete = () => {
-             
-               e.target.parentElement.parentElement.removeChild( e.target.parentElement );
 
-               if(!consultations.firstChild) {
-                   
+               e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+
+               if (!consultations.firstChild) {
+
                     services.textContent = 'Change your visiting hours';
-                   
-                   let noSchedule = document.createElement('p');
-                  
-                   noSchedule.classList.add('text-center');
-                   
-                   noSchedule.textContent = 'No results Found';
-                
+
+                    let noSchedule = document.createElement('p');
+
+                    noSchedule.classList.add('text-center');
+
+                    noSchedule.textContent = 'No results Found';
+
                     consultations.appendChild(noSchedule);
                } else {
-                   services.textContent = 'Cancel your Consultation'
+                    services.textContent = 'Cancel your Consultation'
                }
           }
      }
