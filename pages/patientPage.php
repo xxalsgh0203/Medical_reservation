8e9bdc98d3f854c6a0e79bbb733d483553315a93
Sergio-Source-code <sergiosanzescallon@gmail.com>
2022-04-04 16:12:42 -0500

    Seperated out header and footer into seperate files

--- pages/patientPage.php
+++ pages/patientPage.php
@@ -34,23 +34,7 @@ $email = $row["email"];
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
-<nav class="navbar navbar-expand-lg nav-back fixed-top" id="mainNav">
-  <div class="container">
-    <img src="../img/main_icon.png" class="mainicon">
-    <a class="navbar-brand" href="./main.php">Medimon</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-      data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-      aria-label="Toggle navigation"><i class="fas fa-syringe fa-2x"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="./login.php">Log in</a></li>
-        <li class="nav-item"><a class="nav-link" href="./signup.php">Sign up</a></li>
-        <li class="nav-item"><a class="nav-link" href="./patientPage.php">Manage Reservation</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include_once("../php/header.php"); ?>
 
   <!-- End Header -->
   <!-- ======= Hero Section ======= -->
@@ -66,24 +50,7 @@ $email = $row["email"];
 
 
   <!-- Footer-->
-  <footer class="footer py-4 mt-5">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-4 text-lg-left">COSC3380 Group Project</div>
-        <div class="col-lg-4 my-3 my-lg-0">
-          <a class="btn btn-back btn-social mx-2" href="#!">
-            <i class="fab fa-twitter"></i></a>
-          <a class="btn btn-back btn-social mx-2" href="#!">
-            <i class="fab fa-facebook-f"></i></a>
-          <a class="btn btn-back btn-social mx-2" href="#!">
-            <i class="fab fa-linkedin-in"></i></a>
-        </div>
-        <div class="col-lg-4 text-lg-right">
-          <a class="mr-3 text" href="#!">Privacy Policy</a>
-          <a href="#!" class="text">Terms of Use</a></div>
-      </div>
-    </div>
-  </footer>
+  <?php include_once("../php/footer.php"); ?>
 
   <script src="main.js"></script>
   <!-- Optional JavaScript -->
