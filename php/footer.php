e1df737ac2e4235302b2ed714003b13df7e5e33a
Sergio-Source-code <sergiosanzescallon@gmail.com>
2022-04-04 16:14:22 -0500

    Removed other login options from footer

--- php/footer.php
+++ php/footer.php
@@ -3,16 +3,10 @@
     <div class="row align-items-center">
       <div class="col-lg-4 text-lg-left">COSC3380 Group Project</div>
       <div class="col-lg-4 my-3 my-lg-0">
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-twitter"></i></a>
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-facebook-f"></i></a>
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-linkedin-in"></i></a>
       </div>
       <div class="col-lg-4 text-lg-right">
-        <a class="mr-3 text" href="#!">Privacy Policy</a>
-        <a href="#!" class="text">Terms of Use</a></div>
+        <a class="mr-3 text" href="">Privacy Policy</a>
+        <a href="" class="text">Terms of Use</a></div>
     </div>
   </div>
 </footer>
