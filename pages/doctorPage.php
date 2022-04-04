efd3f84500bdbbd0397d8cc6697fc4effccd3d53
Sergio-Source-code <sergiosanzescallon@gmail.com>
2022-04-04 16:41:39 -0500

    Improved formatting

--- pages/doctorPage.php
+++ pages/doctorPage.php
@@ -15,20 +15,22 @@
 
 <?php include_once("../php/header.php"); ?>
 
-  <!-- End Header -->
-  <!-- ======= Doctor Section ======= -->
-      <body>
-        <div class="text-center" id="login-header">Doctors Page</div>
-        <div class="carousel-container">
-          <div class="carousel-slide">
-              <img src="https://th.bing.com/th/id/OIP.ojEjZxhp8ORBoX0Y2JFbkgHaEM?pid=ImgDet&rs=1" aLt="">
-              <img src="https://th.bing.com/th/id/OIP.JW_4m4RVV4ywf0aiB6TWrgHaLH?w=124&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7" aLt="">
-              <img src="https://pikwizard.com/photos/823b34ec55677202d73fd148bc416087-m.jpg" aLt="">
-          </div>
-        </div>
-       
-        <br>
-        <!--
+<!-- End Header -->
+<!-- ======= Doctor Section ======= -->
+
+<body>
+  <div class="text-center" id="login-header">Doctors Page</div>
+  <div class="carousel-container">
+    <div class="carousel-slide">
+      <img src="https://th.bing.com/th/id/OIP.ojEjZxhp8ORBoX0Y2JFbkgHaEM?pid=ImgDet&rs=1" aLt="">
+      <img src="https://th.bing.com/th/id/OIP.JW_4m4RVV4ywf0aiB6TWrgHaLH?w=124&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+        aLt="">
+      <img src="https://pikwizard.com/photos/823b34ec55677202d73fd148bc416087-m.jpg" aLt="">
+    </div>
+  </div>
+
+  <br>
+  <!--
         <table style="margin-left:auto; margin-right:auto;">
           <tr>
             <th>Doctor ID</th>
@@ -70,25 +72,25 @@
 
 
 
-         
-        <table style="margin-left:auto; margin-right:auto;">
-          <tr>
-            <th>Doctor ID</th>
-            <th>Office ID</th>
-            <th>Specialty</th>
-            <th>Name</th>
-            <th>Availability</th>
-            <th>Phone Number</th>
 
-          </tr>
-        </table> 
-      </body>
-  <!-- End Doctors Page-->
+  <table style="margin-left:auto; margin-right:auto;">
+    <tr>
+      <th>Doctor ID</th>
+      <th>Office ID</th>
+      <th>Specialty</th>
+      <th>Name</th>
+      <th>Availability</th>
+      <th>Phone Number</th>
 
-  <!-- Footer-->
-  <?php include_once("../php/footer.php"); ?>
+    </tr>
+  </table>
+</body>
+<!-- End Doctors Page-->
 
-  <script src="main.js"></script>
+<!-- Footer-->
+<?php include_once("../php/footer.php"); ?>
+
+<script src="main.js"></script>
 </body>
 
 </html>
