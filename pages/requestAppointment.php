dd158cbba26f0ad46db922fbd1936d6dfd593e49
dwolf22 <91570833+dwolf22@users.noreply.github.com>
2022-04-03 20:08:11 -0500

    message

---
+++ pages/requestAppointment.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+ 
+// Check if the user is logged in, if not then redirect them to login page
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//     header("location: login.php");
+//     exit;
+// }
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Request Appointment</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="../css/raStyle.css">
+  <link rel="stylesheet" href="../css/style.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<nav class="navbar navbar-expand-lg nav-back fixed-top" id="mainNav">
+  <div class="container">
+    <img src="../img/main_icon.png" class="mainicon">
+    <a class="navbar-brand" href="./main.html">Medimon</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+      data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+      aria-label="Toggle navigation"><i class="fas fa-syringe fa-2x"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a class="nav-link" href="./login.php">Log in</a></li>
+        <li class="nav-item"><a class="nav-link" href="./signup.php">Sign up</a></li>
+        <li class="nav-item"><a class="nav-link" href="./patientPage.php">Manage Reservation</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+  <!-- End Header -->
+  <!-- ======= Hero Section ======= -->
+<body>
+
+</body>
+  <!-- End Hero -->
+
+
+
+
+  <!-- Footer-->
+  <footer class="footer py-4 mt-5" id="footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-4 text-lg-left">COSC3380 Group Project</div>
+        <div class="col-lg-4 my-3 my-lg-0">
+          <a class="btn btn-back btn-social mx-2" href="#!">
+            <i class="fab fa-twitter"></i></a>
+          <a class="btn btn-back btn-social mx-2" href="#!">
+            <i class="fab fa-facebook-f"></i></a>
+          <a class="btn btn-back btn-social mx-2" href="#!">
+            <i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <div class="col-lg-4 text-lg-right">
+          <a class="mr-3 text" href="#!">Privacy Policy</a>
+          <a href="#!" class="text">Terms of Use</a></div>
+      </div>
+    </div>
+  </footer>
+  </script>
+</body>
+
+</html>
