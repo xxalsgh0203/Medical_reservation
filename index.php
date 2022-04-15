1d2fdaab31ec010199397baf1debf40078bf9cb2
Minho Kim <music4537@naver.com>
2022-04-15 15:57:06 -0500

    css update and done with login logout

--- index.php
+++ index.php
@@ -22,9 +22,10 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
     <div class="container text-center position-relative">
+      <h2>"This is a Medical reservation app made by group 5"</h2>
       <h1>Customoers want to use an app to<br/>schedule appointments with you online</h1>
-      <p>Allow clients to book appointments directly from app - all from one place</p>
-      <h2>Group project COSC3380.</h2>
+      <img id="mainimg" src="./img/mainimage.jpeg" width="50%" height="50%">
+      <p>Allow clients to book appointments directly from app <br/> All From One Place</p>
       <a href="./requestAppointment.php" class="main-btn">Make your Appointment!</a>
     </div>
   </section>
