8c0ad77c5e78702afd4b25f5cb3531d812cd672d
Minho Kim <music4537@naver.com>
2022-04-15 11:46:41 -0500

    logout

--- Login.php
+++ Login.php
@@ -71,7 +71,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="./css/style.css">
-
+  
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -114,8 +114,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!-- Footer-->
 <?php include_once("./php/footer.php"); ?>
-
-<script src="main.js"></script>
 </body>
+
 
 </html>
---
+++ main.js
@@ -0,0 +1,7 @@
+
+const loginbutton = document.getElementById("login-button");
+
+loginbutton.addEventListener('click', ()=>{
+    console.log(1);
+});
+
