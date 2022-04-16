05ddb87a3a5af13ae096396c6b68b4fb0910ae93
Guillermo <reyesguillermo54@yahoo.com>
2022-04-16 14:23:08 -0500

    message

--- adminPages/dataEntryForm.php
+++ adminPages/dataEntryForm.php
@@ -28,7 +28,7 @@ if (isset($_POST['SubmitD']))
     $DPhoneNum = $_POST['DPhoneNum'];
 
     //Used to insert data into doctor
-    $db->query("INSERT INTO DOCTOR (Office_id  Name Speciality Password Phone_number) 
+    $db->query("INSERT INTO DOCTOR (Office_id,  Name, Speciality, Password, Phone_number) 
                     VALUES ('$OFFID', '$DName', '$SPType', '$DPWord', '$DPhoneNum')")  or die($db->error); 
 }
 
