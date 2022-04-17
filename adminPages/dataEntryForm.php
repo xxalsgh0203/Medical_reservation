563927c97d76579fd90958264ad233071d4b17c9
Minho Kim <music4537@naver.com>
2022-04-16 21:32:21 -0500

    medimon icon

--- adminPages/dataEntryForm.php
+++ adminPages/dataEntryForm.php
@@ -104,96 +104,91 @@ if (isset($_POST['SubmitAD']))
   </div>
 </body>
 
-<section id="signup">
+<section id="dataEntry">
+  <form action="" method="POST">
+            <!--input taken for doctor-->
+              <h2>  Add Doctor  </h2>
+              <label for="OFFID">Office ID:</label>
+              <input type="number" id="OFFID" name="OFFID">
+              <label for="SPType">Speciality:</label>
+              <input type="text" id="SPType" name="SPType" maxlength = "30"> 
+              <label for="Dname">Name:</label>
+              <input type="text" id="Dname" name="Dname" maxlength="20">
+              <br>
+              <label for="DPWord">create password:</label>
+              <input type="Password" id="DPWord" name="DPWord">
+              <label for="DPhoneNum">Phone Number:</label>
+              <input type="text" id="DPhoneNum" name="DPhoneNum" maxlength="10">    
+              <!--Used to separate inputs-->
+              <br>
+              <button type="submit" class="btn btn-primary" name="SubmitD">Submit</button>
 
+              <br>  <br>
+            <!--input taken for admin -->
+              <h2>  Add Admin  </h2>
+              <label for="ADOFFID">Office ID:</label>
+              <input type="number" id="ADOFFID" name="ADOFFID">
+              <label for="ADname">Name:</label>
+              <input type="text" id="ADname" name="ADname" maxlength="20">
+              <label for="ADPWord">create password:</label>
+              <input type="text" id="ADPWord" name="ADPWord">
+              <br>
+              <label for="ADPhoneNum">Phone Number:</label>
+              <input type="text" id="ADPhoneNum" name="ADPhoneNum" maxlength="10"> 
+              <label for="ADEmail">Email:</label>
+              <input type="text" id="ADEmail" name="ADEmail" maxlength="30">   
+              <!--Used to separate inputs-->
+              <br>
+              <button type="submit" class="btn btn-primary" name="SubmitAD">Submit</button>
 
-<form action="" method="POST">
-          <!--input taken for doctor-->
-            <h2>  Add Doctor  </h2>
-            <label for="OFFID">Office ID:</label>
-            <input type="number" id="OFFID" name="OFFID">
-            <label for="SPType">Speciality:</label>
-            <input type="text" id="SPType" name="SPType" maxlength = "30"> 
-            <label for="Dname">Name:</label>
-            <input type="text" id="Dname" name="Dname" maxlength="20">
-            <br>
-            <label for="DPWord">create password:</label>
-            <input type="Password" id="DPWord" name="DPWord">
-            <label for="DPhoneNum">Phone Number:</label>
-            <input type="text" id="DPhoneNum" name="DPhoneNum" maxlength="10">    
-            <!--Used to separate inputs-->
-            <br>
-            <button type="submit" class="btn btn-primary" name="SubmitD">Submit</button>
-
-            <br>  <br>
-           <!--input taken for admin -->
-            <h2>  Add Admin  </h2>
-            <label for="ADOFFID">Office ID:</label>
-            <input type="number" id="ADOFFID" name="ADOFFID">
-            <label for="ADname">Name:</label>
-            <input type="text" id="ADname" name="ADname" maxlength="20">
-            <label for="ADPWord">create password:</label>
-            <input type="text" id="ADPWord" name="ADPWord">
-            <br>
-            <label for="ADPhoneNum">Phone Number:</label>
-            <input type="text" id="ADPhoneNum" name="ADPhoneNum" maxlength="10"> 
-            <label for="ADEmail">Email:</label>
-            <input type="text" id="ADEmail" name="ADEmail" maxlength="30">   
-            <!--Used to separate inputs-->
-            <br>
-            <button type="submit" class="btn btn-primary" name="SubmitAD">Submit</button>
-
-             <br>  <br>
-            <!-- Input taken for Patient -->
-            <h2>  Add Patient  </h2>
-            <label for="ADOFFID">Office ID:</label>
-            <input type="number" id="ADOFFID" name="ADOFFID">
-            <label for="ADname">Name:</label>
-            <input type="text" id="ADname" name="ADname" maxlength="20">
-            <label for="ADPWord">create password:</label>
-            <input type="text" id="ADPWord" name="ADPWord">
-            <br>
-            <label for="DPhoneNum">Phone Number:</label>
-            <input type="text" id="ADPhoneNum" name="ADPhoneNum" maxlength="10"> 
-            <label for="ADEmail">Email:</label>
-            <input type="text" id="ADEmail" name="ADEmail" maxlength="30">   
-            <!--Used to separate inputs-->
-            <br>
-            <button type="submit" class="btn btn-primary" name="SubmitAD">Submit</button>
-
-
-
-            <br><br><br>
-            <!--Update row by unique identifier-->
-            <h3> <b>Update Doctor info:</b> </h3>
-            <label for="DPhoneID">Identify Row to Edit:</label>
-            <input type="text" id="DPhoneID" name ="DPhoneID" maxlength="10">
-            <br>
-            <label for="UOFFID">Office ID:</label>
-            <input type="number" id="UOFFID" name="UOFFID">
-            <label for="USPType">Speciality:</label>
-            <input type="text" id="USPType" name="USPType" maxlength = "30"> 
-            <label for="UDname">Name:</label>
-            <input type="text" id="UDname" name="UDname" maxlength="20">
-            <br>
-            <label for="UDPWord">change password:</label>
-            <input type="Password" id="UDPWord" name="UDPWord">
-            <label for="UDPhoneNum">Phone Number:</label>
-            <input type="text" id="UDPhoneNum" name="UDPhoneNum" maxlength="10">
-            <br>
-            <button type="submit" class="btn btn-primary" name="USubmitD">Submit</button>
-
-            <br><br><br>
-            <!--Delete doctor row -->
-            <h3> <b>Delete Doctor row:</b> </h3>
-            <label for="DPhoneID">Identify Row to Edit:</label>
-            <input type="text" id="DPhoneID" name ="DPhoneID" maxlength="10">
-            <br>
-            <button type="submit" class="btn btn-primary" name="DelSubmitD">Delete</button>
-</form>
+              <br>  <br>
+              <!-- Input taken for Patient -->
+              <h2>  Add Patient  </h2>
+              <label for="ADOFFID">Office ID:</label>
+              <input type="number" id="ADOFFID" name="ADOFFID">
+              <label for="ADname">Name:</label>
+              <input type="text" id="ADname" name="ADname" maxlength="20">
+              <label for="ADPWord">create password:</label>
+              <input type="text" id="ADPWord" name="ADPWord">
+              <br>
+              <label for="DPhoneNum">Phone Number:</label>
+              <input type="text" id="ADPhoneNum" name="ADPhoneNum" maxlength="10"> 
+              <label for="ADEmail">Email:</label>
+              <input type="text" id="ADEmail" name="ADEmail" maxlength="30">   
+              <!--Used to separate inputs-->
+              <br>
+              <button type="submit" class="btn btn-primary" name="SubmitAD">Submit</button>
 
 
 
+              <br><br><br>
+              <!--Update row by unique identifier-->
+              <h3> <b>Update Doctor info:</b> </h3>
+              <label for="DPhoneID">Identify Row to Edit:</label>
+              <input type="text" id="DPhoneID" name ="DPhoneID" maxlength="10">
+              <br>
+              <label for="UOFFID">Office ID:</label>
+              <input type="number" id="UOFFID" name="UOFFID">
+              <label for="USPType">Speciality:</label>
+              <input type="text" id="USPType" name="USPType" maxlength = "30"> 
+              <label for="UDname">Name:</label>
+              <input type="text" id="UDname" name="UDname" maxlength="20">
+              <br>
+              <label for="UDPWord">change password:</label>
+              <input type="Password" id="UDPWord" name="UDPWord">
+              <label for="UDPhoneNum">Phone Number:</label>
+              <input type="text" id="UDPhoneNum" name="UDPhoneNum" maxlength="10">
+              <br>
+              <button type="submit" class="btn btn-primary" name="USubmitD">Submit</button>
+
+              <br><br><br>
+              <!--Delete doctor row -->
+              <h3> <b>Delete Doctor row:</b> </h3>
+              <label for="DPhoneID">Identify Row to Edit:</label>
+              <input type="text" id="DPhoneID" name ="DPhoneID" maxlength="10">
+              <br>
+              <button type="submit" class="btn btn-primary" name="DelSubmitD">Delete</button>
+  </form>
 </section>
 
 
