8480ada00ece1a8e237ce40a9ed13c29c084707d
Minho Kim <music4537@naver.com>
2022-04-20 17:03:02 -0500

    remove footer

--- patientPages/requestAppointment2.php
+++ patientPages/requestAppointment2.php
@@ -116,24 +116,7 @@ where Speciality = 'Oncologist';*/
     </div>
 </body>
 <!-- Footer-->
-<footer class="footer py-4 mt-5">
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-4 text-lg-left">COSC3380 Group Project</div>
-      <div class="col-lg-4 my-3 my-lg-0">
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-twitter"></i></a>
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-facebook-f"></i></a>
-        <a class="btn btn-back btn-social mx-2" href="#!">
-          <i class="fab fa-linkedin-in"></i></a>
-      </div>
-      <div class="col-lg-4 text-lg-right">
-        <a class="mr-3 text" href="#!">Privacy Policy</a>
-        <a href="#!" class="text">Terms of Use</a></div>
-    </div>
-  </div>
-</footer>
+
 
 <script src="main.js"></script>
 
