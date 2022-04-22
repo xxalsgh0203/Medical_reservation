d92ab478fe21a33ac8a60b44811b6b25a5594d2c
Guillermo <reyesguillermo54@yahoo.com>
2022-04-22 14:07:21 -0500

    message

--- adminPages/SpecialistApproval.php
+++ adminPages/SpecialistApproval.php
@@ -2,6 +2,29 @@
 session_start();
 
 require_once "../php/config.php";
+
+
+//Query to retrieve appointments for doctors
+$sql = "SELECT PData.NAME, PData.Email , PData.Phone_Number, PData.Age, PData.Specialist_approved, DData.NAME, DData.Email , DData.Phone_Number FROM PATIENT PData LEFT JOIN DOCTOR DData";
+$result = mysqli_query($db, $sql);
+
+
+$PtableResult = "";
+if ($result->num_rows > 0) {
+  while($row = $result-> fetch_assoc()) {
+    $APtableResult .= "<tr>". "<td>" . $row["Office_id"] . "</td><td>"  . $row["Office_id"] . "</td><td>" .
+                       $row["Appointment_status"] . "</td><td>" . $row["Slotted_time"] . "</td><td>" . $row["Specialist_status"] . "</td>";
+                       
+    if ($row["Appointment_status"] === 'pending') {
+      $APtableResult .= "<td><a href='../adminPages/adminPage.php?approve_id=" . $row["Appointment_id"] . "'>X</a></td>";
+      $APtableResult .= "<td><a href='../adminPages/adminPage.php?ren  ject_id=" . $row["Appointment_id"] . "'>X</a></td>";
+    } else {
+      $APtableResult .= "<td></td><td></td>";
+    }
+    $APtableResult .= "</tr>";
+
+  }
+}
 ?>
 
 <!doctype html>
