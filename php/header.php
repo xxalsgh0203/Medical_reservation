4cdda7470afe193921dd31c2f848104c4504be03
Minho Kim <music4537@naver.com>
2022-04-25 20:44:48 -0500

    link update

--- php/header.php
+++ php/header.php
@@ -10,8 +10,8 @@ if (isset($_SESSION["href"])) {
 
 <nav class="navbar navbar-expand-lg nav-back fixed-top" id="mainNav">
   <div class="container">
-    <img src="./img/main_icon.png" class="mainicon" href="//medical-service.infinityfreeapp.com/index.php">
-    <a class="navbar-brand" href=<?php "//medical-service.infinityfreeapp.com/index.php"?>>Medimon</a>
+    <img src="./img/main_icon.png" class="mainicon" href="medical-service.infinityfreeapp.com">
+    <a class="navbar-brand" href=<?php "medical-service.infinityfreeapp.com"?>>Medimon</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
       data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
       aria-label="Toggle navigation"><i class="fas fa-syringe fa-2x"></i>
